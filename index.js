7070b79a33482615a1975dd272bb8d61c4cd94c3
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-08-28 14:21:11 +0530

    data added

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
-  console.log("JSON Server is running");
+  console.log("JSON Server is running", port);
 });
